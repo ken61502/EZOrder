b27a800f251d593132300175f7e0b8fff9090477
ken61502 <kennyson@gmail.com>
2014-05-12 22:47:14 +0800

    Init Ezorder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ezorder::Application.config.secret_key_base = 'aad43f239e4a5beb74aa85e4c2ddf7c8b1c0ee1cc38c8c7bcfa36a857a01764469ad9ab666592a48e69d4fb5a3610bca35bbc9e084e91b6b1945f823be104cab'
